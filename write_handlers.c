41e1a189603958110dd0fb5dfc4d95e30df5b972
vrxmike <alvinvrxmike@ivloud.com>
2022-08-11 22:23:21 -0700

    11-Task

--- write_handlers.c
+++ write_handlers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /************************* WRITE HANDLE *************************/
-
 /**
  * handle_write_char - Prints a string
  * @c: char types.
@@ -10,9 +9,9 @@
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
+ *
  * Return: Number of chars printed.
  */
-
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
 { /* char is stored at left and paddind at buffer's right */
@@ -46,7 +45,6 @@ int handle_write_char(char c, char buffer[],
 }
 
 /************************* WRITE NUMBER *************************/
-
 /**
  * write_number - Prints a string
  * @is_negative: Lista of arguments
@@ -56,9 +54,9 @@ int handle_write_char(char c, char buffer[],
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
+ *
  * Return: Number of chars printed.
  */
-
 int write_number(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -90,9 +88,9 @@ int write_number(int is_negative, int ind, char buffer[],
  * @length: Number length
  * @padd: Pading char
  * @extra_c: Extra char
+ *
  * Return: Number of printed chars.
  */
-
 int write_num(int ind, char buffer[],
 	int flags, int width, int prec,
 	int length, char padd, char extra_c)
@@ -148,9 +146,9 @@ int write_num(int ind, char buffer[],
  * @width: Width specifier
  * @precision: Precision specifier
  * @size: Size specifier
+ *
  * Return: Number of written chars.
  */
-
 int write_unsgnd(int is_negative, int ind,
 	char buffer[],
 	int flags, int width, int precision, int size)
@@ -207,9 +205,9 @@ int write_unsgnd(int is_negative, int ind,
  * @padd: Char representing the padding
  * @extra_c: Char representing extra char
  * @padd_start: Index at which padding should start
+ *
  * Return: Number of written chars.
  */
-
 int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start)
 {
