f21f184a732d4e860f86e82dda054bdcf5d7c74d
vrxmike <alvinvrxmike@ivloud.com>
2022-08-11 22:16:21 -0700

    9-Task

--- utils.c
+++ utils.c
@@ -3,9 +3,9 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
+ *
  * Return: 1 if c is printable, 0 otherwise
  */
-
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
@@ -21,13 +21,10 @@ int is_printable(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-
 	/* The hexa format code is always 2 digits long */
-
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -43,9 +40,9 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
+ *
  * Return: 1 if c is a digit, 0 otherwise
  */
-
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -58,9 +55,9 @@ int is_digit(char c)
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
+ *
  * Return: Casted value of num
  */
-
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
@@ -75,9 +72,9 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
+ *
  * Return: Casted value of num
  */
-
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
