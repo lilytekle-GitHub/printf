7665e17a26a6353ca855763d529141fec174b499
vrxmike <alvinvrxmike@ivloud.com>
2022-08-11 19:49:46 -0700

    0-Task

--- _printf.c
+++ _printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
@@ -8,7 +7,6 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format.
  * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -26,7 +24,6 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
-
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
@@ -60,7 +57,6 @@ int _printf(const char *format, ...)
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
